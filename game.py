3d546dc4dd86e315a121b9586f2312532c3d193f
NeanSegura <neansegura@hotmail.com>
2024-07-02 17:06:46 -0300

    feat: adds escape button for quitting

--- game.py
+++ game.py
@@ -54,7 +54,7 @@ class Game:
 
         events = pygame.event.get()
         for event in events:
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                 pygame.quit()
                 sys.exit()
 
